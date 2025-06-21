941e64a80f90d124340f2bea78c1a804829d47c7
Raphael <rewoma19@gmail.com>
2025-06-20 18:33:32 -0600

    Complete code for freelancer rates challenge

--- freelancer_rates/freelancer-rates.js
+++ freelancer_rates/freelancer-rates.js
@@ -8,4 +8,18 @@ function daysInBudget(budget, ratePerHour) {
   return Math.floor(budget / dailyRate);
 }
 
-function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {}
+function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
+  const dailyRate = dayRate(ratePerHour);
+  const monthlyRate = dailyRate * 22; // remember that there are 22 billable days
+
+  // Number of months based on each full month having 22 billable days
+  const fullMonths = Math.floor(numDays / 22);
+
+  const daysLeft = numDays % 22; // number of days asides the 22 billable days in a month
+
+  const rateForFullMonths = fullMonths * monthlyRate;
+  const discountedMonthlyRate = rateForFullMonths * (1 - discount);
+  const rateForDaysLeft = dailyRate * daysLeft;
+  const totalPriceWithMonthlyDiscount = discountedMonthlyRate + rateForDaysLeft;
+  return Math.ceil(totalPriceWithMonthlyDiscount);
+}
