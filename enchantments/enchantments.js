920d7fedda9486c3e643080d0f3612d88bf43faa
Raphael <rewoma19@gmail.com>
2025-06-23 16:03:49 -0600

    Add code for 2nd enchantments task

--- enchantments/enchantments.js
+++ enchantments/enchantments.js
@@ -2,3 +2,8 @@ function getItem(cards, position) {
   const pickedCard = cards[position];
   return pickedCard;
 }
+
+function setItem(cards, postion, replacementCard) {
+  cards[postion] = replacementCard;
+  return cards;
+}
