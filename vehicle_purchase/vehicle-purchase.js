5cdb58b7fdbe5654be980da3af02619db3bf974a
Raphael <rewoma19@gmail.com>
2025-06-23 12:24:12 -0600

    Add code for final vehicle purchase task

--- vehicle_purchase/vehicle-purchase.js
+++ vehicle_purchase/vehicle-purchase.js
@@ -22,3 +22,20 @@ function chooseVehicle(option1, option2) {
   const decisionStatement = chosenOption + " is clearly the better choice.";
   return decisionStatement;
 }
+
+function calculateResellPrice(originalPrice, age) {
+  const lessThan3YearsOld = age < 3;
+  const moreThan10YeardOld = age > 10;
+  const between3And10YearsOld = 3 <= age <= 10;
+  let resellPrice;
+
+  if (lessThan3YearsOld) {
+    resellPrice = 0.8 * originalPrice;
+  } else if (moreThan10YeardOld) {
+    resellPrice = 0.5 * originalPrice;
+  } else if (between3And10YearsOld) {
+    resellPrice = 0.7 * originalPrice;
+  }
+
+  return resellPrice;
+}
