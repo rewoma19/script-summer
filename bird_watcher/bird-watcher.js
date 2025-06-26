05a6fe957381fb28022c31abbb3a2eaff7d44463
Raphael <rewoma19@gmail.com>
2025-06-26 13:06:16 -0600

    Add code for 2nd bird watcher task

--- bird_watcher/bird-watcher.js
+++ bird_watcher/bird-watcher.js
@@ -7,3 +7,14 @@ function totalBirdCount(birdsPerDay) {
 
   return totalNumOfBirds;
 }
+
+function birdsInWeek(birdsPerDay, week) {
+  const startDay = (week - 1) * 7;
+  let totalBirdsInWeek = 0;
+
+  for (let i = startDay; i < startDay + 7; i++) {
+    totalBirdsInWeek += birdsPerDay[i];
+  }
+
+  return totalBirdsInWeek;
+}
