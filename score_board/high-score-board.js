c30e5c3a6bc7d361862e88d64dc2f423cda8b2ef
Raphael <rewoma19@gmail.com>
2025-06-27 16:29:39 -0700

    Add code for 3rd high score board task

--- score_board/high-score-board.js
+++ score_board/high-score-board.js
@@ -10,3 +10,11 @@ function addPlayer(scoreBoard, player, score) {
   scoreBoard[player] = score;
   return scoreBoard;
 }
+
+function removePlayer(scoreBoard, player) {
+  if (scoreBoard.hasOwnProperty(player)) {
+    delete scoreBoard[player];
+  }
+
+  return scoreBoard;
+}
