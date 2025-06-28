8cddbac1fa63220b7480d1b4809ac12740205d33
Raphael <rewoma19@gmail.com>
2025-06-28 14:16:25 -0600

    Add code for 3rd amusement park task

--- amusement_park/amusement-park.js
+++ amusement_park/amusement-park.js
@@ -12,3 +12,18 @@ function revokeTicket(visitor) {
   visitor.tickedId = null;
   return visitor;
 }
+
+function ticketStatus(tickets, ticketId) {
+  let status;
+
+  // check if ticketId property does not exist in tickets object
+  if (!tickets.hasOwnProperty(ticketId)) {
+    status = "unknown ticket id";
+  } else if (tickets[ticketId] === null) {
+    status = "not sold";
+  } else {
+    status = `sold to ${tickets[ticketId]}`;
+  }
+
+  return status;
+}
