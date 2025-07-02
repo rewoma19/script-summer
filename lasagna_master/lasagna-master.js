cb3217cb99cf1f9f0f70018e2568e6334ec9dc15
Raphael <rewoma19@gmail.com>
2025-07-02 01:36:50 -0600

    Add code for 2nd lasagna master task

--- lasagna_master/lasagna-master.js
+++ lasagna_master/lasagna-master.js
@@ -17,3 +17,15 @@ function cookingStatus(remainingTime) {
 // Test cases
 console.log(cookingStatus(12));
 console.log(cookingStatus());
+
+function preparationTime(layers, avgPrepTimePerLayer = 2) {
+  const numOfLayers = layers.length;
+  const prepTime = numOfLayers * avgPrepTimePerLayer;
+
+  return prepTime;
+}
+
+// Test cases
+const layers = ["sauce", "noodles", "sauce", "meat", "mozzarella", "noodles"];
+console.log(preparationTime(layers, 3));
+console.log(preparationTime(layers));
