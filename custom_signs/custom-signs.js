6d8fd0ea81651161d0d4f3efd2b197a0f888ad21
Raphael <rewoma19@gmail.com>
2025-07-04 00:28:58 -0600

    Add code for final custom signs task

--- custom_signs/custom-signs.js
+++ custom_signs/custom-signs.js
@@ -19,3 +19,14 @@ Class of ${year}`;
 
   return gradSign;
 }
+
+function costOf(sign, currency) {
+  let costOfSign = 20;
+
+  for (let char of sign) {
+    costOfSign += 2;
+  }
+
+  const costStatement = `Your sign costs ${costOfSign.toFixed(2)} ${currency}.`;
+  return costStatement;
+}
