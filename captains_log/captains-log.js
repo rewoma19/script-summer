857abe0843859403fa8bfb562a6d557d4b0d9d6d
Raphael <rewoma19@gmail.com>
2025-07-06 11:22:53 -0600

    Add code for final captains log task and test cases

--- captains_log/captains-log.js
+++ captains_log/captains-log.js
@@ -9,7 +9,7 @@ function randomShipRegistryNumber() {
 }
 
 // Test case
-randomShipRegistryNumber();
+console.log(randomShipRegistryNumber());
 
 // Generate a random stardate.
 
@@ -17,3 +17,17 @@ function randomStardate() {
   const randomStar = 41000.0 + Math.random() * (42000.0 - 41000.0);
   return randomStar;
 }
+
+// Test case
+console.log(randomStardate());
+
+function randomPlanetClass() {
+  const planetaryClasses = ["D", "H", "J", "K", "L", "M", "N", "R", "T", "Y"];
+  //   Generate an index for the random planet that will be returned from the planetaryClasses array
+  const randomPlanetIndex = Math.floor(Math.random() * planetaryClasses.length);
+  const randomPlanet = planetaryClasses[randomPlanetIndex];
+  return randomPlanet;
+}
+
+// Test case
+console.log(randomPlanetClass());
