9255ca6150efe17d581b2491cff9d34dd07c4482
Raphael <rewoma19@gmail.com>
2025-07-06 12:00:54 -0600

    Add code for 1st destructures enchantments task

---
+++ destructured_enchantments/enchantments.js
@@ -0,0 +1,9 @@
+// Get the first card in the given deck.
+function getFirstCard(deck) {
+  const [firstCard] = deck;
+  return firstCard;
+}
+
+// Test cases
+const deck = [5, 9, 7, 1, 8];
+console.log(getFirstCard(deck));
