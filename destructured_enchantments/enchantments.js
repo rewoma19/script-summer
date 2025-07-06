8c7bd58e7bfb6a46d001a8819b8f173ad24cb9db
Raphael <rewoma19@gmail.com>
2025-07-06 13:01:55 -0600

    Add code for 2nd destructured enchantments task

--- destructured_enchantments/enchantments.js
+++ destructured_enchantments/enchantments.js
@@ -7,3 +7,13 @@ function getFirstCard(deck) {
 // Test cases
 const deck = [5, 9, 7, 1, 8];
 console.log(getFirstCard(deck));
+
+// Get the second card in the given deck.
+function getSecondCard(deck) {
+  const [_, secondCard] = deck;
+  return secondCard;
+}
+
+// Test cases
+const deck2 = [3, 2, 19, 6, 7];
+console.log(getSecondCard(deck2));
