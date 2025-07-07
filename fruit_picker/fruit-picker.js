6689280285ed0fe0e3bbe0ebf9cb7e4e27de3d7c
Raphael <rewoma19@gmail.com>
2025-07-07 07:06:19 -0600

    Add code for 2nd fruit picker task

--- fruit_picker/fruit-picker.js
+++ fruit_picker/fruit-picker.js
@@ -1,8 +1,15 @@
 import { notify } from "./notifier";
 
 function onSuccess() {
+  // implement the onSuccess callback to call notify with a success message
   const successMessage = { message: "SUCCESS" };
   notify(successMessage);
 }
 
 // onSuccess();
+
+function onError() {
+  // implement the onError callback to call notify with an error message
+  const errMessage = { message: "ERROR" };
+  notify(errMessage);
+}
