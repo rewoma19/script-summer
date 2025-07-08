6ea935053648c331b95fbc7b118961e2b976b417
Raphael <rewoma19@gmail.com>
2025-07-07 22:48:35 -0700

    Add code for final looping enchantments task

--- looping_enchantments/enchantments.js
+++ looping_enchantments/enchantments.js
@@ -11,3 +11,22 @@ function cardTypeCheck(stack, card) {
 
   return numOfCardsOfType;
 }
+
+// Determine how many cards are odd or even
+function determineOddEvenCards(stack, type) {
+  let numOfEvenCards = 0;
+  let numOfOddCards = 0;
+
+  for (let card of stack) {
+    if (card % 2 === 0) {
+      numOfEvenCards++;
+    }
+
+    if (card % 2 !== 0) {
+      numOfOddCards++;
+    }
+  }
+
+  const numOfCardsToReturn = type === true ? numOfEvenCards : numOfOddCards;
+  return numOfCardsToReturn;
+}
