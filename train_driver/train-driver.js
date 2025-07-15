72db1b71cb9a87c041ee0e96929e68479c11261a
Raphael <rewoma19@gmail.com>
2025-07-14 19:19:46 -0600

    Add code for 2nd train driver task

--- train_driver/train-driver.js
+++ train_driver/train-driver.js
@@ -5,3 +5,14 @@ function getListOfWagons(...wagonIDs) {
 
 // Test Case
 console.log(getListOfWagons(1, 7, 12, 3, 14, 8, 5));
+
+// Reorder the array of wagons by moving the first 2 wagons to the end of the array.
+function fixListOfWagons(ids) {
+  const [wagon1, wagon2, ...allOtherWagons] = ids;
+  const fixedListOfWagons = [...allOtherWagons, wagon1, wagon2];
+  return fixedListOfWagons;
+}
+
+// Test Case
+const eachWagonsID = [2, 5, 1, 7, 4, 12, 6, 3, 13];
+console.log(fixListOfWagons(eachWagonsID));
