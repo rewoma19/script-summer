fae4720e35e6a4c385d69b38ec32ce5051f5dcdb
Raphael <rewoma19@gmail.com>
2025-07-15 15:32:21 -0600

    Add code for 2nd transformative enchantments task

--- transformative_enchantments/enchantments.js
+++ transformative_enchantments/enchantments.js
@@ -5,3 +5,23 @@ function seeingDouble(deck) {
   const doubledDeck = deck.map((card) => card * 2);
   return doubledDeck;
 }
+
+// Create triplicates of every 3 found in the deck.
+function threeOfEachThree(deck) {
+  // loop over each card in the deck
+  for (let i = 0; i < deck.length; i++) {
+    // check if the current card in the iteration has a value of 3
+    if (deck[i] === 3) {
+      // if so, at the index/position after that of the card, add 2 cards with values of 3 in front
+      deck.splice(i + 1, 0, 3, 3);
+      // skip over the two added cards with values of 3
+      i += 2;
+    }
+  }
+
+  return deck;
+}
+
+// Test case
+const deck = [1, 3, 9, 3, 7];
+console.log(threeOfEachThree(deck));
